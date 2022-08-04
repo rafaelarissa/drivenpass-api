10c089fe50cff62398f3343d1fdd18a38427ac1e
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-04 14:56:06 -0300

    Preparando revisão de código

--- src/schemas/wifiSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const wifiSchema = Joi.object({
-  name: Joi.string().required(),
-  password: Joi.string().required(),
-  title: Joi.string().required(),
-});
-
-export default wifiSchema;
